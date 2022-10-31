359d2deee373a88b5d1f9e5dc8bbf0a4eb16b7dd
Irina <irina.delving@yandex.ru>
2022-11-01 00:58:11 +0300

    add tack1

---
+++ WORK/GeekBrains_DZ/Seminar2/tack1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int division10 = 0;
+int result = 0;
+
+if (num > 99 && num < 1000)
+    {
+        division10 = num / 10;
+        result = division10 % 10;
+     Console.WriteLine($"{num} вторая цифра {result}");
+    }
+
+else {
+     Console.WriteLine("Число не трехзначное");
+
+}
+
