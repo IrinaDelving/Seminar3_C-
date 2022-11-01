294309ac7bc25761668dfb4118887e2f9585e537
Irina <irina.delving@yandex.ru>
2022-11-02 00:27:41 +0300

    add task3

---
+++ WORK/GeekBrains_DZ/Seminar4/task3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int size = 8;
+int [] array = new int [8];
+Random rnd = new Random();
+int count = array.Length;
+  
+    for (int i=0; i < size; i++)
+    {
+    array [i] = rnd.Next(9);
+    Console.Write(array[i]);
+    }
+  
+
+   
+
+
+
