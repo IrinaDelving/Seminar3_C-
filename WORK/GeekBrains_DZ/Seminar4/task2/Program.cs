c0ce0d6cf04c0ef2c3766ce2aa1f979bb23f07bd
Irina <irina.delving@yandex.ru>
2022-11-02 21:05:05 +0300

    add task2

---
+++ WORK/GeekBrains_DZ/Seminar4/task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Введите число, ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int digit = 0;
+
+while (num > 0)
+{
+    digit = num % 10;
+    sum = sum + digit;
+    num = num /10;
+}
+Console.WriteLine($"Сумма цифр в числе равна {sum}");
