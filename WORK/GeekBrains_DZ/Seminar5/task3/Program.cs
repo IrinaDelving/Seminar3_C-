6c4916e8d98976c75959b4691a6c195b08f5558d
Irina <irina.delving@yandex.ru>
2022-11-06 18:53:01 +0300

    add task3

---
+++ WORK/GeekBrains_DZ/Seminar5/task3/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+//[3 7 22 2 78] -> 76
+
+int [] myArray = {3, 7, 22, 2, 78, 42,5};
+int minValue = myArray[0];
+int maxValue = myArray[0];
+
+for (int i = 1; i < myArray.Length; i++)
+{
+    if (myArray[i] < minValue)
+    {
+        minValue = myArray[i];
+    }
+}
+Console.WriteLine(minValue);
+
+for (int i = 1; i < myArray.Length; i++)
+{
+    if (myArray[i] > maxValue)
+    {
+       maxValue = myArray[i];
+    }
+}
+Console.WriteLine(maxValue);
+Console.WriteLine("Разница максимального и минимального: " +(maxValue -  minValue));
+
+
+
+
+
+
+
+
+
+
+
