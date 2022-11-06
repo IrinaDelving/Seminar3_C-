8b921a83953994b94b577d10c4cac53c1a0404eb
Irina <irina.delving@yandex.ru>
2022-11-07 00:04:35 +0300

    add task1

---
+++ WORK/GeekBrains_DZ/Seminar5/task1/Program.cs
@@ -0,0 +1,25 @@
+﻿//  Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
+//которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+Random myRandom = new Random();
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int [] numbers = new int [size];
+int count = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = myRandom.Next(100, 1000);
+    Console.Write(numbers[i] + " ");
+}
+
+for (int i = 0; i < numbers.Length; i ++)
+{
+   if (numbers[i] % 2 == 0) count++; 
+}
+Console.WriteLine($"всего {numbers.Length} чисел, {count} из них чётные");
+
+
+
+
