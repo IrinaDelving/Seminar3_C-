c0bdbbe231f1a8fcd3ef1b9527b611273eacecee
Irina <irina.delving@yandex.ru>
2022-11-21 18:41:16 +0300

    add task1

---
+++ WORK/GeekBrains_DZ/Seminar9/task1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+int SetNumber(string str)
+{
+    System.Console.WriteLine(str);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+int n = SetNumber("Введите число");
+
+static String recursion(int n) 
+{
+if (n==1)
+{
+  return "1";  
+}    
+return recursion(n - 1) + " " + n;
+}
+{
+    Console.WriteLine((recursion(n))); 
+}
+    
+recursion(n);
+
+
+
+
+
+
